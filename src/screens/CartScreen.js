5dbe2f179b16fe97d777d621b46fdc6fb4d678b1
JazibEqbal <jazibeqbal0526@gmail.com>
2023-05-07 22:25:31 +0530

    cart screen

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,36 +1,56 @@
 import React, { useContext, useEffect, useState } from "react";
 import ShopContext from "../context/ShopContext";
 import { useParams } from "react-router-dom";
-import { Row, Col} from 'react-bootstrap';
+import { Alert } from "react-bootstrap";
+import Loading from "../components/Loading";
 
 const CartScreen = () => {
   const { shopInstance } = useContext(ShopContext);
   const [product, setProduct] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   let { id } = useParams();
 
-  const qtyParams = new URLSearchParams(window.location.search).get("qty");
+  const setLoaderTiming = () => {
+    setTimeout(() => {
+      setIsLoading(!isLoading);
+    }, 150);
+  };
+  // const qtyParams = new URLSearchParams(window.location.search).get("qty");
   useEffect(() => {
-    const getCartProductById = async () => {
-      const { data } = await shopInstance.getAProductById(id);
+    const getCartProduct = async () => {
+      const { data } = await shopInstance.getCart();
       setProduct(data);
+      setLoaderTiming();
     };
-    getCartProductById();
+    getCartProduct();
   }, [setProduct, id, shopInstance]);
 
-  return <div>
-    {/* <Row>
-      <Col md={8}>
-         <h1>Shopping Cart</h1>
-         {items.length === 0 ? <Message></Message>}
-      </Col>
-      <Col md={2}>
-        
-        </Col>
-        <Col md={2}>
-        
-        </Col>
-    </Row> */}
-  </div>;
+  return (
+    <div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          {product.length === 0 ? (
+            <Alert className="text-white p-2 text-uppercase col-3 text-center bg-danger">OOPs!! Your Cart is Empty</Alert>
+          ) : (
+            <div>
+              {product.map((prod) => (
+                <div key={prod._id}>
+                  <h2>{prod.name}</h2>
+                  <img src={prod.image} alt="img" />
+                  <h2>{prod.brand}</h2>
+                  <h2>{prod.price}</h2>
+                  <h2>{prod.quantity}</h2>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default CartScreen;
